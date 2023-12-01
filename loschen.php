bbc67afeaf4c2d4160d8fff7e7cb22208545a8c7
DagonJeff <134562648+DagonJeff@users.noreply.github.com>
2023-12-01 00:50:01 -0300

    Comentários em loschen.php

--- loschen.php
+++ loschen.php
@@ -1,5 +1,9 @@
 <?php
+
+//Assim como no index e na página que salva os dados é chamada a conexão aqui.
     include_once "verbindung.php";
+
+//Semelhante a página "fertig" que salva os dados ele utiliza o método GET porém aqui só é necessário "pegar" o dado do código.
 
     $cod = filter_input(INPUT_GET, "codigo", FILTER_SANITIZE_SPECIAL_CHARS);
 
@@ -7,6 +11,7 @@
     $sql = "DELETE FROM produtos WHERE id = $cod";
 
 
+//A mesma coisa da página "fertig" só que aqui ele recebe a variável com o comando SQL "DELETE FROM".
 
     $loschen = mysqli_query($verbindung, $sql);
 
